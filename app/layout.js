53741eb8ae20146b17d42cb4842ddae65076c35d
Biraj Karki <birajkarki9849@gmail.com>
2024-01-04 18:44:15 +0545

    added the company form with formik and implemented the rtk to it and display in table

---
+++ app/layout.js
@@ -0,0 +1,22 @@
+"use client";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { Provider } from "react-redux";
+import store from "@/redux/store";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {" "}
+        <Provider store={store}>
+          <Navbar />
+          {children}
+        </Provider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,10 @@
+import CompanyForm from "@/components/CompanyForm";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <CompanyForm />
+    </>
+  );
+}
